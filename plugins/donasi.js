78d0cf7da7dacf3db1f288236ca10b0f87528884
Lans turu ygy <111067376+Lans-ofc@users.noreply.github.com>
2023-03-18 18:36:29 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -8,13 +8,13 @@ let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? c
 let Poto = fs.readFileSync('./media/menu.jpg')
 let tek = `▷ ᴅᴏɴᴀsɪ ʙɪsᴀ ᴍᴇʟᴀʟᴜɪ ◁
     「 Donasi • Pulsa 」
-▪ *Telkomsel:* [0822 1472 9677]
-▪ *Axis:* [0838 6372 7401]
+▪ *Telkomsel:* [0821 3586 5472]
+▪ *Axis:* [0831 3274 9084]
 
     「 Donasi • Non Pulsa 」
-▪ *Dana:* [0838 6373 7401]
-▫ *Gopay:* [-]
-▪ *Ovo:* [0838 6372 7401]
+▪ *Dana:* [0831 3274 9074]
+▫ *Gopay:* [0831 3274 9084]
+▪ *Ovo:* [-]
 
 Kalau Mau Pake Qr Bisa ke contact Owner kak☺️
 wa.me/6283863727401
@@ -23,7 +23,7 @@ wa.me/6283863727401
 
 let cap = `Donasi Nya Kak ${tag} ☺️`
 conn.send3ButtonImg(m.chat, Poto, cap, tek, 'Owner', '.owner', 'Sewa Bot', '.sewa', `\nOwner Bot Ganteng Banget`, '.owner', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: 'https://www.instagram.com/l4c1f2r',
+    mediaUrl: 'https://www.instagram.com/lans.xys',
     mediaType: 2, 
     description: sgc,
     title: 'ᴅ ᴏ ɴ ᴀ ᴛ ᴇ',
