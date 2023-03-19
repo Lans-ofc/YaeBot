b48e657ee222d846078f9da5ccd45fa2eba1b8d2
Lans turu ygy <111067376+Lans-ofc@users.noreply.github.com>
2023-03-19 21:50:45 +0700

    Update creator2.js

--- plugins/creator2.js
+++ plugins/creator2.js
@@ -5,15 +5,15 @@ let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
 let name = await conn.getName(who)
 
 if (command == 'creator') {
- let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp; LansModz\nNICKNAME:👑 Owner Yaemiko Bot\nORG: Amelia XD\nTITLE:soft\nitem1.TEL;waid=628313249084:+62 831-3274-9084\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://lynk.id/na_offical\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET: achmadmaulanaamirudin@gmail.com\nitem3.X-ABLabel:💌 Mail Owner\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 ,02 Juni 2007\nEND:VCARD`
+ let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp; LansModz\nNICKNAME:👑 Owner Yaemiko Bot\nORG: Na_Devlopers\nTITLE:soft\nitem1.TEL;waid=628313249084:+62 831-3274-9084\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://lynk.id/na_offical\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET: achmadmaulanaamirudin@gmail.com\nitem3.X-ABLabel:💌 Mail Owner\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 ,02 Juni 2007\nEND:VCARD`
 const tag_own = await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fkontak })
-let caption = `👋 Hai *@${who.split("@")[0]}*, Nih Owner Saya kak`
-    await conn.sendButton(m.chat, caption, author, null, [['👨Sapa Owner', 'Huuu']], m, { quoted: fkontak, mentions: conn.parseMention(caption) })
+let caption = `Halo *@${who.split("@")[0]}*, Nih Pemilik/creator bot nya`
+    await conn.sendButton(m.chat, caption, author, null, [['Sapa Owner', 'Huuu']], m, { quoted: fkontak, mentions: conn.parseMention(caption) })
 }
 if (command == 'own') {
-  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;${author};;;\nFN:${author}\nORG:${author}\nTITLE:\nitem1.TEL;waid=6283863727401:+62 838-6372-7401\nitem1.X-ABLabel:${author}\nX-WA-BIZ-DESCRIPTION:${htjava} Nih pengembang ku kack yg mengaktifkan aq.\nX-WA-BIZ-NAME:${author}\nEND:VCARD`
+  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;${author};;;\nFN:${author}\nORG:${author}\nTITLE:\nitem1.TEL;waid=628313249084:+62 831-3274-9084\nitem1.X-ABLabel:${author}\nX-WA-BIZ-DESCRIPTION:${htjava} Nih pengembang ku kack yg mengaktifkan aq.\nX-WA-BIZ-NAME:${author}\nEND:VCARD`
 await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fkontak })
-await conn.reply(m.chat, `Halo kak @${m.sender.split(`@`)[0]} itu nomor ownerku , jangan di spam ya `,m)
+await conn.reply(m.chat, `Halo kak @${m.sender.split(`@`)[0]} Jika ingin ada yang di tanyakan bisa chat , No spam  `,m)
   }
 }
 handler.help = ['owner', 'creator']
