fb4a931ae08392e4c58acf5fa9aff923fd490472
Lans turu ygy <111067376+Lans-ofc@users.noreply.github.com>
2023-03-19 23:07:42 +0700

    Update p.js

--- plugins/p.js
+++ plugins/p.js
@@ -3,6 +3,7 @@ import { sticker } from '../lib/sticker.js'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
 let stiker = await sticker(null, global.API(`${pickRandom(stikerhuuu)}`), global.packname, global.author)
+conn.fakeReply(m.chat, stiker, '0@s.whatsapp.net', '*YAEMIKO-BOTZ🗿*', 'status@broadcast')
     if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
     throw stiker.toString()
     
@@ -17,5 +18,5 @@ function pickRandom(list) {
 }
 let stikerhuuu = [
 
-"https://telegra.ph/file/a3408b1aeb9066dedaaba.jpg",
+"https://telegra.ph/file/9fcb3be52fbc9ea266e3e.jpg",
 ]
